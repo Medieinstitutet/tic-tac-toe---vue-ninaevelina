d85be88413f554f518d869bd7e960723409c594b
Nina Evelina Khodnia <khodnia.ninaevelina@gmail.com>
2023-04-17 21:37:00 +0200

    remove AddPlayer-component from App.vue

--- src/components/AddPlayer.vue
+++ src/components/AddPlayer.vue
@@ -4,19 +4,42 @@ import { ref } from 'vue';
 
 
 const symbol = ref("X");
+const player = ref("")
+
 
 const emit = defineEmits(["addPlayer"])
 
 const addPlayer = () => {
-    emit('addPlayer', symbol.value);
-    symbol.value = "";
+    emit('addPlayer', player.value);
+    symbol.value = "X";
+    player.value = "";
+    console.log(player.value);
+
 }
 
 </script>
 
 <template>
     <form @submit.prevent="addPlayer">
-    <input v-model="symbol" type="text"/>
+    <input v-model="player" type="text"/>
     <button>+</button>
     </form>
 </template>
+
+<style scoped>
+
+form {
+    display: flex;
+}
+
+input {
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    border: 3px solid black;
+}
+button {
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    background-color: black;
+    color: white;
+}
+
+</style>
